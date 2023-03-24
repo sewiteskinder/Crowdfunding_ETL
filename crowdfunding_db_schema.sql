630d758f79c7201b777de98f068120a50ffd0195
sewiteskinder <sewit.eskinder@gmail.com>
2023-03-23 19:54:11 -0500

    completed sql schema and ERD

---
+++ crowdfunding_db_schema.sql
@@ -0,0 +1,65 @@
+-- Drop tables if they exist
+DROP TABLE category;
+
+DROP TABLE subcategory;
+
+DROP TABLE campaign;
+
+DROP TABLE contacts;
+
+-- Create category table
+CREATE TABLE category (
+	category_id VARCHAR NOT NULL,
+	category VARCHAR NOT NULL,
+	PRIMARY KEY (category_id)
+);
+
+-- View table
+SELECT * FROM category;
+
+-- Create subcategory table
+CREATE TABLE subcategory (
+	subcategory_id VARCHAR NOT NULL,
+	subcategory VARCHAR NOT NULL,
+	PRIMARY KEY (subcategory_id)
+);
+
+-- View table
+SELECT * FROM subcategory;
+
+-- Create contacts table
+CREATE TABLE contacts (
+	contact_id INT NOT NULL,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	email VARCHAR NOT NULL,
+	PRIMARY KEY (contact_id)
+);
+
+-- View table
+SELECT * FROM contacts;
+
+-- Create campaign table
+CREATE TABLE campaign (
+	cf_id INT NOT NULL,
+	contact_id INT NOT NULL,
+	company_name VARCHAR NOT NULL,
+	description VARCHAR NOT NULL,
+	goal FLOAT NOT NULL,
+	pledged FLOAT NOT NULL,
+	outcome VARCHAR NOT NULL,
+	backers_count VARCHAR NOT NULL,
+	country VARCHAR NOT NULL,
+	currency VARCHAR NOT NULL,
+	launched_date DATE NOT NULL,
+	end_date DATE NOT NULL,
+	category_id VARCHAR NOT NULL,
+	subcategory_id VARCHAR NOT NULL,
+	PRIMARY KEY (cf_id),
+	FOREIGN KEY (category_id) REFERENCES category(category_id),
+	FOREIGN KEY (subcategory_id) REFERENCES subcategory(subcategory_id),
+	FOREIGN KEY (contact_id) REFERENCES contacts(contact_id)
+);
+
+-- View table
+SELECT * FROM campaign;
